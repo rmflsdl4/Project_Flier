155fe8a97d0255f97b91a78e1d1903484b2ba992
rmflsdl4 <57540594+rmflsdl4@users.noreply.github.com>
2023-07-11 00:53:03 +0900

    Add files via upload

--- PHP/SignUp.php
+++ PHP/SignUp.php
@@ -7,27 +7,42 @@
         $confirm_pw = $_POST['confirm_pw'];
         $nick_name = $_POST['nick_name'];
 
-        $query = "INSERT INTO users(id, pw, nick_name) VALUES (?, ? ,?)";
-
+        $query = "SELECT * FROM users WHERE id = {$id}";
         $stmt = mysqli_prepare($conn, $query);
-        
-        mysqli_stmt_bind_param($stmt, 'sss', $id, $pw, $nick_name);
+
+        mysqli_stmt_bind_param($stmt, 's', $id);
 
         $result = mysqli_stmt_execute($stmt);
 
-        if (mysqli_affected_rows($conn) > 0) {
+        if(mysqli_num_rows($result) > 0){
+            mysqli_stmt_close($stmt);
+            mysqli_close($conn);
             echo "<script>
-            alert('회원가입이 완료되었습니다.');
+            alert('회원가입에 실패하였습니다.');
+            window.location.href = 'https://web-project-flier-k19y2kljvm1qyo.sel4.cloudtype.app/Login.html';
             </script>";
+            exit;
         }
         else{
-            echo "<script>alert('회원가입에 실패하였습니다.');</script>";
+            $query = "INSERT INTO users(id, pw, nick_name) VALUES (?, ? ,?)";
+
+            $stmt = mysqli_prepare($conn, $query);
+            
+            mysqli_stmt_bind_param($stmt, 'sss', $id, $pw, $nick_name);
+    
+            $result = mysqli_stmt_execute($stmt);
+    
+            if (mysqli_affected_rows($conn) > 0) {
+                echo "<script>
+                alert('회원가입이 완료되었습니다.');
+                </script>";
+            }
+            
+            mysqli_stmt_close($stmt);
+            mysqli_close($conn);
+            echo "<script>
+            window.location.href = 'https://web-project-flier-k19y2kljvm1qyo.sel4.cloudtype.app/Login.html';
+            </script>";
         }
-        
-        mysqli_stmt_close($stmt);
-        mysqli_close($conn);
-        echo "<script>
-        window.location.href = 'https://web-project-flier-k19y2kljvm1qyo.sel4.cloudtype.app/Login.html';
-        </script>";
     }
 ?>
