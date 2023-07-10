d53e466636a9337a192d85bd99b8bb2d081b1764
rmflsdl4 <57540594+rmflsdl4@users.noreply.github.com>
2023-07-11 00:25:58 +0900

    Add files via upload

--- PHP/SignUp.php
+++ PHP/SignUp.php
@@ -6,24 +6,25 @@
         $pw = $_POST['pw'];
         $confirm_pw = $_POST['confirm_pw'];
         $nick_name = $_POST['nick_name'];
-        echo $id;
-        $query = "INSERT INTO users(id, pw, nick_nme) VALUES (?, ? ,?)";
+        
+        $query = "INSERT INTO users(id, pw, nick_name) VALUES (?, ? ,?)";
 
         $stmt = mysqli_prepare($conn, $query);
         
         mysqli_stmt_bind_param($stmt, 'sss', $id, $pw, $nick_name);
 
-        $result = mysqli_stmt_excute($stmt);
+        $result = mysqli_stmt_execute($stmt);
 
         if (mysqli_affected_rows($conn) > 0) {
             echo "<script>alert('회원가입이 완료되었습니다.');</script>";
-            header('../HTML/Login.html');
+            mysqli_stmt_close($stmt);
+            mysqli_close($conn);
+            header("Location: ../HTML/Login.html");
         }
         else{
+            mysqli_stmt_close($stmt);
+            mysqli_close($conn);
             echo "<script>alert('회원가입에 실패하였습니다.');</script>";
         }
-
-        mysqli_stmt_close($stmt);
-        mysqli_close($conn);
     }
 ?>
