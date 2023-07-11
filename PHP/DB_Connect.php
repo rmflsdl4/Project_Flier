0648581984cb320b50804118edd7f1714a43924d
rmflsdl4 <57540594+rmflsdl4@users.noreply.github.com>
2023-07-11 22:30:07 +0900

    Delete PHP directory

--- PHP/DB_Connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $host = 'svc.sel4.cloudtype.app';
-    $user = 'root';
-    $password = 'tkfkdgo3@';
-    $database = 'flier';
-    $port = '32388';
-
-    $conn = new mysqli($host, $user, $password, $database, $port);
-?>
