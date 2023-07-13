a74c5550b91fb443e1852485d20b4fe949f3093f
rmflsdl4 <57540594+rmflsdl4@users.noreply.github.com>
2023-07-14 01:34:39 +0900

    Add files via upload

--- NodeJS/JavaScript/Login.js
+++ NodeJS/JavaScript/Login.js
@@ -3,7 +3,7 @@ const database = require('../database.js');
 async function _Login(id, pw){
     await database.Connect();
     
-    const query = 'SELECT * FROM users WHERE id = ? AND pw = ?';
+    const query = 'SELECT COUNT(*) as count FROM users WHERE id = ? AND pw = ?';
     const values = [id, pw];
 
     const result = await database.Query(query, values);
@@ -14,6 +14,7 @@ async function _Login(id, pw){
         
     
     database.Close();
+    return result[0].count;
 }
 
 module.exports = {
