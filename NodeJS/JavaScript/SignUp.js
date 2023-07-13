45b12b766da8041bff3c4adcf9919484902bdc6a
rmflsdl4 <57540594+rmflsdl4@users.noreply.github.com>
2023-07-13 23:14:05 +0900

    Add files via upload

--- NodeJS/JavaScript/SignUp.js
+++ NodeJS/JavaScript/SignUp.js
@@ -1,6 +1,21 @@
 const database = require('../database.js');
-const normalization = require('./JavaScript/Normalization_Check.js');
 
-async function Field_Value_Check(){
+async function User_Insert(id, pw, nick_name){
+    await database.Connect();
     
+    if(id_normal.test(value)){
+        const query = 'INSERT INTO users(id, pw, nick_name) VALUES (?, ?, ?)';
+        const values = [id, pw, nick_name];
+
+        const result = await database.Query(query, values);
+
+        if (result instanceof Error) {
+            return;
+        }
+    };
+    database.Close();
 }
+
+module.exports = {
+    Add_User: User_Insert
+};
