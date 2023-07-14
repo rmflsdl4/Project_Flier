c15840cbb292e2e7da6bfbf9090dca9eb2d7d6f9
rmflsdl4 <57540594+rmflsdl4@users.noreply.github.com>
2023-07-15 03:54:06 +0900

    HTML 전체 갱신

--- NodeJS/HTML/JavaScript/Board.js
+++ NodeJS/HTML/JavaScript/Board.js
@@ -1,22 +1,20 @@
 const menu = document.getElementsByClassName('Board_Menu');
-const table = document.getElementById('Board');
-let tr = document.createElement("tr");
-let td = document.createElement("td");
 
-
-function Board_Select(){
+async function Board_Select(){
     const clickElement = event.target;
 
     for(let idx = 0; idx < menu.length; idx++){
         if(menu[idx] === clickElement){
             menu[idx].style.opacity = 1;
+            await Posts_Output(menu[idx].textContent);
         }
         else{
             menu[idx].style.opacity = 0.2;
         }
     }
 }
-function Board_State_Init(){
+async function Board_State_Init(){
+    
     for(let idx = 0; idx < menu.length; idx++){
         if(idx === 0){
             menu[idx].style.opacity = 1;
@@ -25,12 +23,36 @@ function Board_State_Init(){
             menu[idx].style.opacity = 0.2;
         }
     }
-    let posts = Posts_Import();
-    console.log(posts);
-    //table.appendChild(tr);
-    //tr.appendChild(td);
+    await Posts_Output('공지사항');
 }
+async function Posts_Output(board_type){
+    const board = document.getElementById('Board');
+    const tds = document.getElementsByClassName('add_td_Tag');
 
+    let posts = await Posts_Import();
+    let rows = [];
+    while(tds.length > 0){
+        tds[0].remove();
+    }
+    for(let idx = 0; idx < posts.length; idx++){
+        let row = posts[idx];
+        if(row['board_type'] === board_type){
+            rows.push(row);
+        }
+    }
+    for(let idx = 0; idx < rows.length; idx++){
+        const tr = document.createElement('tr');
+        const row = rows[idx];
+        let structure = `
+            <td class='add_td_Tag' colspan='1'>${idx + 1}</td>
+            <td class='add_td_Tag' colspan='4'>${row['title']}</td>
+            <td class='add_td_Tag' colspan='2'>${row['author_id']}</td>
+            <td class='add_td_Tag' colspan='2'>${row['date']}</td>
+            <td class='add_td_Tag' colspan='1'>${row['view_count']}</td>`;
+        board.appendChild(tr);
+        tr.innerHTML = structure;
+    }
+}
 function Posts_Import() {
     return new Promise((resolve, reject) => {
         fetch('/posts-import', {
