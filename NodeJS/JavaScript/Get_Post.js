1b71deb39100b40a6d21c0cfd1f8accc888569f2
rmflsdl4 <57540594+rmflsdl4@users.noreply.github.com>
2023-07-15 03:54:39 +0900

    모듈 갱신

--- NodeJS/JavaScript/Get_Post.js
+++ NodeJS/JavaScript/Get_Post.js
@@ -3,18 +3,19 @@ const database = require('../database.js');
 async function Get_Post(){
     await database.Connect();
     
-    const query = 'SELECT title, author_id, added, view_count, board_type FROM posts';
+    const query = 'SELECT post_id, title, author_id, DATE_FORMAT(added, \'%Y-%m-%d %H:%i:%s\') as date, view_count, board_type FROM posts';
 
     const result = await database.Query(query, null);
 
     if (result instanceof Error) {
         return;
     }
-        
-    
+
     database.Close();
     return result;
 }
+
+
 module.exports = {
     Get: Get_Post
 };
