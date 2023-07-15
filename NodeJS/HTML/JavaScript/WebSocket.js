0808349b70611c65cbf81f8d4a5cd60cfa8b5de6
rmflsdl4 <57540594+rmflsdl4@users.noreply.github.com>
2023-07-16 00:13:15 +0900

    Update WebSocket.js

--- NodeJS/HTML/JavaScript/WebSocket.js
+++ NodeJS/HTML/JavaScript/WebSocket.js
@@ -1,5 +1,8 @@
 const webSocket = new WebSocket(`ws://${window.location.hostname}:2098`);
 
+webSocket.onopen =  function(){
+    console.log('접속 성공');
+}
 
 function Client_Count(){
     const clists = document.getElementById('client_cnt');
