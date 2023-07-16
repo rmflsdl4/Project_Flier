8d6d9f2245a631a9f42c44c35e4a23dc4d78c478
rmflsdl4 <57540594+rmflsdl4@users.noreply.github.com>
2023-07-17 01:19:00 +0900

    Add files via upload

--- NodeJS/JavaScript/Post.js
+++ NodeJS/JavaScript/Post.js
@@ -31,9 +31,22 @@ async function Add_View_Count(post_id){
         return;
     } 
 }
+async function Add_Post(board_type, title, content, user_id){
+    const query = `INSERT INTO posts(title, author_id, board_type, content) VALUES (?, ?, ?, ?)`;
+
+    const values = [title, user_id, board_type, content];
+
+    result = await database.Query(query, values);
+    
+
+    if (result instanceof Error) {
+        return;
+    } 
+}
 
 module.exports = {
     Get_List: Get_Post_List,
     Get_Post: Get_Post,
-    Add_View_Count: Add_View_Count
+    Add_View_Count: Add_View_Count,
+    Add_Post: Add_Post
 };
