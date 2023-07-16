07dfd6a185488d3c4504e41441c299b65f4381b5
rmflsdl4 <57540594+rmflsdl4@users.noreply.github.com>
2023-07-17 01:17:38 +0900

    Add files via upload

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,6 +1,6 @@
 // 사용 모듈 로드
 const express = require('express');
-const wsModule = require('ws');
+const session = require('express-session');
 const normalization = require('./JavaScript/Normalization_Check.js');
 const signup = require('./JavaScript/SignUp.js');
 const login = require('./JavaScript/Login.js');
@@ -14,6 +14,14 @@ var fs = require('fs');
 app.use(express.static('HTML'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+    },
+}));
 
 // 서버 구동
 app.listen(2098, function(){
@@ -76,11 +84,17 @@ app.post('/check-input', (req, res) => {
 app.post('/sign-up', (req, res) => {
     const { id, pw, nick_name } = req.body;
 
-    signup.Add_User(id, pw, nick_name);
+    try{
+        signup.Add_User(id, pw, nick_name);
     
-    console.log(`신규 회원 정보 [ ID - ${id} / NAME - ${nick_name} ]`);
+        console.log(`신규 회원 정보 [ ID - ${id} / NAME - ${nick_name} ]`);
 
-    res.send("<script>alert('회원가입이 완료되었습니다.'); location.href='Login.html';</script>");
+        res.send("<script>alert('회원가입이 완료되었습니다.'); location.href='Login.html';</script>");
+    }
+    catch(error){
+        console.error('회원가입 오류:', error);
+        res.status(500).send("<script>alert('회원가입에 실패하였습니다.'); location.href='SignUp.html';</script>");
+    }
 });
 app.post('/login', (req, res) => {
     const now = new Date();
@@ -98,8 +112,9 @@ app.post('/login', (req, res) => {
     login.Login(id, pw)
         .then((state) => {
             if(state === 1){
+                req.session.user_id = id;
                 console.log(`회원 [ ${id} ] 접속.... 접속 시간 : ${formattedDate}`);
-        
+                console.log(`세션에 ID 저장: ${req.session.user_id}`);
                 res.send("<script>alert('로그인에 성공하였습니다.'); location.href='Main.html';</script>");
             }
             else{
@@ -118,4 +133,17 @@ app.post('/view-post', async (req, res) => {
     await posts.Add_View_Count(post_id);
     const data = await posts.Get_Post(post_id);
     res.send(data);
+})
+
+app.post('/add-post', async (req, res) => {
+    const { board_type, title, content } = req.body;
+    try{
+        console.log(req.session.user_id);
+        await posts.Add_Post(board_type, title, content, req.session.user_id);
+        res.send("<script>alert('게시글을 등록하였습니다.'); location.href='Main.html';</script>");
+    }
+    catch(error){
+        console.log(error);
+        res.send("<script>alert('게시글 등록에 실패하였습니다.'); location.href='Main.html';</script>");
+    }
 })
