3c096430d33e0ff63e82a5a6bde0160b387c758f
rmflsdl4 <57540594+rmflsdl4@users.noreply.github.com>
2023-07-17 19:36:55 +0900

    수정

--- NodeJS/JavaScript/Post.js
+++ NodeJS/JavaScript/Post.js
@@ -2,7 +2,7 @@ const database = require('../database.js');
 
 async function Get_Post_List(){
     
-    const query = `SELECT post_id, title, CONCAT(nick_name, '(', SUBSTRING(id, 1, 5), '***', ')') as author_id, DATE_FORMAT(added, '%Y-%m-%d %H:%i:%s') as date, view_count, board_type
+    const query = `SELECT post_id, title, CONCAT(nick_name, '(', SUBSTRING(id, 1, 5), '***', ')') as author_id, DATE_FORMAT(added, '%Y-%m-%d %H:%i:%s') as date, view_count, board_type, user_type, lock_bool
                     FROM posts
                     JOIN users
                     ON posts.author_id = users.id`;
@@ -16,7 +16,11 @@ async function Get_Post_List(){
     return result;
 }
 async function Get_Post(post_id){
-    const query = `SELECT title, author_id, DATE_FORMAT(added, \'%Y-%m-%d %H:%i:%s\') as date, view_count, board_type, content FROM posts WHERE post_id = ${post_id}`;
+    const query = `SELECT title, author_id, DATE_FORMAT(added, \'%Y-%m-%d %H:%i:%s\') as date, view_count, board_type, content, user_type, lock_bool
+                    FROM posts 
+                    JOIN users
+                    ON posts.author_id = users.id
+                    WHERE post_id = ${post_id}`;
 
     const result = await database.Query(query, null);
 
@@ -34,10 +38,10 @@ async function Add_View_Count(post_id){
         return;
     } 
 }
-async function Add_Post(board_type, title, content, user_id){
-    const query = `INSERT INTO posts(title, author_id, board_type, content) VALUES (?, ?, ?, ?)`;
+async function Add_Post(board_type, title, content, user_id, lock_state){
+    const query = `INSERT INTO posts(title, author_id, board_type, content, lock_bool) VALUES (?, ?, ?, ?, ?)`;
 
-    const values = [title, user_id, board_type, content];
+    const values = [title, user_id, board_type, content, lock_state];
 
     result = await database.Query(query, values);
     
@@ -46,10 +50,36 @@ async function Add_Post(board_type, title, content, user_id){
         return;
     } 
 }
+async function Lock_Post_Check(post_id, user_id){
+    const query_1 = `SELECT COUNT(*) as count
+                    FROM posts
+                    JOIN users
+                    ON posts.author_id = users.id
+                    WHERE post_id = ? AND author_id = ?`;
+
+    const query_2 = `SELECT user_type FROM users WHERE id = ?`;
+    const values = [post_id, user_id];
+
+    result_1 = await database.Query(query_1, values);
+    result_2 = await database.Query(query_2, user_id);
+    if (result_1 instanceof Error) {
+        return;
+    }
+    if (result_2 instanceof Error) {
+        return;
+    }
+    if(result_1[0].count == 1 || result_2[0].user_type === 'admin'){
+        return 'true';
+    }
+    else{
+        return 'false';
+    }
+}
 
 module.exports = {
     Get_List: Get_Post_List,
     Get_Post: Get_Post,
     Add_View_Count: Add_View_Count,
-    Add_Post: Add_Post
+    Add_Post: Add_Post,
+    Lock_Check: Lock_Post_Check
 };
