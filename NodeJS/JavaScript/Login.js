d3142227784e9faba99627a73b673fa2eef2c972
rmflsdl4 <57540594+rmflsdl4@users.noreply.github.com>
2023-07-20 21:02:11 +0900

    함수 추가

--- NodeJS/JavaScript/Login.js
+++ NodeJS/JavaScript/Login.js
@@ -6,15 +6,28 @@ async function _Login(id, pw){
     const values = [id, pw];
 
     const result = await database.Query(query, values);
+    const user_id = result[0].count;
 
     if (result instanceof Error) {
         return;
     }
-        
-    
-    return result[0].count;
+    const user_type = await User_Type_Check(id);
+    const arr = [user_id, user_type];
+    console.log(arr);
+    return arr;
 }
+async function User_Type_Check(user_id){
+    const query = `SELECT user_type FROM users WHERE id = ?`;
+    try{
 
+        const result = await database.Query(query, user_id);
+
+        return result[0].user_type;
+    }
+    catch(error){
+        console.log(error);
+    }
+}
 module.exports = {
     Login: _Login
 };
